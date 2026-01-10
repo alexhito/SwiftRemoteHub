63356828e9574a2295ba2032e325fef34f123b47
Alexander <alexander@MacBook-Air-de-Alexander.local>
2026-01-10 13:41:35 -0500

    Initial SwiftUI project with base folder structure

---
+++ App/SwiftRemoreHubApp.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftRemoreHubApp.swift
+//  SwiftRemoteHub
+//
+//  Created by Alexander on 10/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRemoteHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello!")
+        }
+    }
+}
+
+
+#Preview {
+  ContentView()
+}
